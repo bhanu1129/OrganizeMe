83b443d0ffebfc47c49550afd140732f903ef813
bhanu1129 <bhanubansal913@gmail.com>
2023-10-05 00:16:42 +0530

    calculator-fix

--- src/pages/Calculator.js
+++ src/pages/Calculator.js
@@ -6,14 +6,6 @@ import { create, all } from 'mathjs';
 const Calculator = () => {
 
   const math = create(all);
-  math.import({
-    'import':     function () { throw new Error('Function import is disabled') },
-    'createUnit': function () { throw new Error('Function createUnit is disabled') },
-    'evaluate':   function () { throw new Error('Function evaluate is disabled') },
-    'parse':      function () { throw new Error('Function parse is disabled') },
-    'simplify':   function () { throw new Error('Function simplify is disabled') },
-    'derivative': function () { throw new Error('Function derivative is disabled') }
-  }, { override: true })
 
   const navigate = useNavigate();
   const [value, setValue] = useState('');
